0a4609e6eafd7f444e6d7a5ec314607e3158a9aa
Shreya Kundu <shreyak4821@gmail.com>
2024-08-07 19:28:56 +0530

    hi shreya

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import Company_card from "../Components/Company_card";
+// import Gallery_Card from "../Components/Gallery_card";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Company_card/>
+      {/* <Gallery_Card /> */}
       <Footer/>
     </>
   );
 }
-
---
+++ src/components/Gallery_card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Gallery_Card (){
+  return (
+    <div className="rounded-md border-2 border-purple-500 p-4 w-28 ">
+      <div className="rounded-full bg-slate-500 p-40">
+        <h1 className="text-white font-bold text-xl text-center">Gallery Cards</h1>
+      </div>
+    </div>
+  );
+};
