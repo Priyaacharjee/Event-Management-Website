48f442a9c2effde33b1c9fae837aad861e4224c1
Shreya Kundu <shreyak4821@gmail.com>
2024-08-08 09:41:16 +0530

    good morning

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,64 +13,72 @@ import {
 export default function Home() {
   return (
     <>
+        {/* ----------------------------------------------------------------------------- HEADER ------------------------------------------------------------- */}
       <Header />
+
+        {/* -----------------------------------------------------------------------------EVENT TYPES --------------------------------------------------------- */}
       <div className="bg-slate-900 w-full h-80 px-32 py-20">
         {/* event cards */}
       </div>
 
-      <div className="justify-center items-center flex-col flex">
-        <div className="flex py-10 px-20 items-center h-96">
-          <div className="w-3/5">Play run & Analyze you Events</div>
-          <div className="">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries.
-          </div>
-        </div>
+        {/* ------------------------------------------------------------------------------- FEATURES --------------------------------------------------------- */}
+      <div id="features" className="justify-center items-center flex-col flex">
+            <div className="flex py-10 px-20 items-center h-96">
+              <div className="w-3/5">Play run & Analyze you Events</div>
+              <div className="">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived not
+                only five centuries.
+              </div>
+            </div>
 
-        <div className="w-[90%] bg-gradient-to-r from-[#011936] to-[#243b55] h-80 rounded-xl px-32 py-20">
-          {/* Gallery crd slider */}
-        </div>
+        {/*--------------------------------------------------------------------------   GALLERY -------------------------------------------------------------  */}
+            <div className="w-[90%] bg-gradient-to-r from-[#011936] to-[#243b55] h-80 rounded-xl px-32 py-20">
+              {/* Gallery crd slider */}
+            </div>
 
-        <div className="h-auto"><Company_card/></div>
+        {/*------------------------------------------------------------------------  COLLABORATORS    ------------------------------------------------------  */}
+            <div className="h-auto"><Company_card/></div>
 
-        <div className="w-[90%] bg-gradient-to-r from-[#355070] to-[#6D597A] h-60 rounded-xl px-32 py-10 flex text-white">
-          {/* Analytics */}
-          <div className="flex flex-col w-[25%] h-full justify-center pl-24  space-y-[2px]">
-            <div>
-              <FontAwesomeIcon
-                icon={faCalendarCheck}
-                style={{ color: "#ffffff" }}
-              />
+        {/*------------------------------------------------------------------------  ANALYTICS   -----------------------------------------------------------  */}
+            <div id="analytics" className="w-[90%] bg-gradient-to-r from-[#355070] to-[#6D597A] h-60 rounded-xl px-32 py-10 flex text-white mb-10">
+              {/* Analytics */}
+              <div className="flex flex-col w-[25%] h-full justify-center pl-24  space-y-[2px]">
+                <div>
+                  <FontAwesomeIcon
+                    icon={faCalendarCheck}
+                    style={{ color: "#ffffff" }}
+                  />
+                </div>
+                <div className="text-3xl font-bold">100,000+</div>
+                <div>Events</div>
+              </div>
+              <div className="flex flex-col w-[25%] h-full justify-center pl-24  space-y-[2px]">
+                <div>
+                  <FontAwesomeIcon icon={faCalendar} style={{ color: "#ffffff" }} />
+                </div>
+                <div className="text-3xl font-bold">50,000+</div>
+                <div>Event Palnner</div>
+              </div>
+              <div className="flex flex-col w-[25%] h-full justify-center pl-24  space-y-[2px]">
+                <div>
+                  <FontAwesomeIcon icon={faGlobe} style={{ color: "#ffffff" }} />
+                </div>
+                <div className="text-3xl font-bold">100+</div>
+                <div>Country</div>
+              </div>
+              <div className="flex flex-col w-[25%] h-full justify-center pl-24  space-y-[2px]">
+                <div>
+                  <FontAwesomeIcon icon={faUsers} style={{ color: "#ffffff" }} />
+                </div>
+                <div className="text-3xl font-bold">100K+</div>
+                <div>Attendees</div>
+              </div>
             </div>
-            <div className="text-3xl font-bold">100,000+</div>
-            <div>Events</div>
-          </div>
-          <div className="flex flex-col w-[25%] h-full justify-center pl-24  space-y-[2px]">
-            <div>
-              <FontAwesomeIcon icon={faCalendar} style={{ color: "#ffffff" }} />
-            </div>
-            <div className="text-3xl font-bold">50,000+</div>
-            <div>Event Palnner</div>
-          </div>
-          <div className="flex flex-col w-[25%] h-full justify-center pl-24  space-y-[2px]">
-            <div>
-              <FontAwesomeIcon icon={faGlobe} style={{ color: "#ffffff" }} />
-            </div>
-            <div className="text-3xl font-bold">100+</div>
-            <div>Country</div>
-          </div>
-          <div className="flex flex-col w-[25%] h-full justify-center pl-24  space-y-[2px]">
-            <div>
-              <FontAwesomeIcon icon={faUsers} style={{ color: "#ffffff" }} />
-            </div>
-            <div className="text-3xl font-bold">100K+</div>
-            <div>Attendees</div>
-          </div>
-        </div>
       </div>
+        {/*------------------------------------------------------------------------  FOOTER   -----------------------------------------------------------  */}
       <Footer/>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,66 +1,53 @@
+
 import React, { useState } from "react";
-import ThemeButton from "./ThemeButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBars,
-  faXmark,
-  faCaretDown,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark, faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import bgImage from "../assets/bg.jpeg";
 
 export default function Header() {
   const [hamburgerMenuClicked, setHamburgerMenuClicked] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
 
   const hambergerClick = () => {
-    setHamburgerMenuClicked(!hamburgerMenuClicked);
+    if (hamburgerMenuClicked) {
+      setIsClosing(true);
+      setTimeout(() => {
+        setHamburgerMenuClicked(false);
+        setIsClosing(false);
+      }, 1000); 
+    } else {
+      setHamburgerMenuClicked(true);
+    }
   };
 
   return (
     <>
-      <nav className="bg-blue-950 h-12 flex items-center px-4 justify-center w-auto">
-        {/* <ThemeButton /> */}
+      <nav id="header" className="bg-blue-950 h-12 flex items-center px-4 justify-center w-auto">
+        {/* Logo */}
+        <div className="w-1/5 text-white lg:pl-15 xl:pl-20">Logo</div>
 
-        {/* Logo----------------------------------------------------------------------------- */}
-        <div className="w-1/5 text-white lg:pl-15 xl:pl-20 ">Logo</div>
-
-        {/* Navbar Manu----------------------------------------------------------------------------- */}
-        <div className="items-center w-3/5 ">
+        {/* Navbar Menu */}
+        <div className="items-center w-3/5">
           <ul className="invisible sm:visible md:visible lg:visible xl:visible 2xl:visible text-white flex text-center">
-            {/* <li></li> */}
-            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline items-center w-1/5">
-              Home
-            </li>
-            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline w-1/5">
-              Services
-            </li>
-            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline w-1/5">
-              About
-            </li>
-            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline w-1/5">
-              Contact
-            </li>
-            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline p w-1/5">
-              Gallery
-            </li>
+            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline items-center w-1/5">Home</li>
+            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline w-1/5">Services</li>
+            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline w-1/5">About</li>
+            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline w-1/5">Contact</li>
+            <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline w-1/5">Gallery</li>
           </ul>
         </div>
-        {/* Login Button----------------------------------------------------------------------------- */}
-        {/* <button className="rounded-md bg-slate-100 px-3 items-center py-1 ml-auto mr-5 xl:mr-16 2xl:mr-20">Login</button> */}
 
-        {/* User logo----------------------------------------------------------------------------- */}
+        {/* User Logo */}
         <div className="w-1/5 flex justify-end px-3 py-1 space-x-3 2xl:pr-20 xl:pr-8 lg:pr-3 md:pr-2 sm:pr-0 items-center pr-5">
           <img
             src="https://cdn-icons-png.freepik.com/512/219/219986.png"
-            alt=""
-            title="User Image"
+            alt="User"
             className="rounded-xl h-6 w-6 md:h-8 md:w-8 lg:h-8 lg:w-8 xl:h-8 xl:w-8 2xl:h-8 2xl:w-8 bg-blue-300 sm:h-6 sm:w-6 hover:cursor-pointer"
-          ></img>
-          <span className="text-white font-bold hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline pr-5">
-            User
-          </span>
+          />
+          <span className="text-white font-bold hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline pr-5">User</span>
           <FontAwesomeIcon icon={faCaretDown} style={{ color: "#ffffff" }} />
 
-          {/* Hamburger Menu------------------------------------------------------------------------- */}
+          {/* Hamburger Menu */}
           {hamburgerMenuClicked ? (
             <FontAwesomeIcon
               icon={faXmark}
@@ -79,32 +66,25 @@ export default function Header() {
         </div>
       </nav>
 
-      {/* Menu in phone screen------------------------------------------------------------------------- */}
-
+      {/* Menu in Phone Screen */}
       <div
-        className="w-full flex h-96 justify-end bg-cover"
+        className={`w-full flex h-96 justify-end bg-cover`}
         style={{ backgroundImage: `url(${bgImage})` }}
-      >
-        {hamburgerMenuClicked ? (
-          <div className="flex-col flex justify-end bg-blue-950 text-white w-40 items-center h-[12.2rem] animate-[slideIn_1s_ease-in-out]">
-            <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline">
-              Home
-            </div>
-            <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline">
-              Services
-            </div>
-            <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline">
-              About
-            </div>
-            <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline">
-              Contact
-            </div>
-            <div className="w-full text-center pt-2 pb-2 hover:cursor-pointer hover:text-blue-300 hover:underline">
-              Gallery
-            </div>
+        ><div
+        className={`w-full flex  justify-end bg-cover ${hamburgerMenuClicked ? 'animate-slideIn' : isClosing ? 'animate-slideOut' : 'hidden'}`}
+      ></div>
+     
+        {(hamburgerMenuClicked || isClosing) && (
+          <div className={`flex-col flex justify-end bg-gradient-to-r from-blue-600 to-cyan-900 mt-4 text-white w-40 items-center h-[12.2rem] ${isClosing ? 'animate-slideOut' : 'animate-slideIn'}`}>
+                <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline">Home</div>
+                <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline">Services</div>
+                <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline">About</div>
+                <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline">Contact</div>
+                <div className="w-full text-center pt-2 pb-2 hover:cursor-pointer hover:text-blue-300 hover:underline">Gallery</div>
           </div>
-        ) : null}
+        )}
       </div>
     </>
   );
 }
+
