359b1a8774e221a532404df795af6f54436a04c3
Keya Tarafdar <keya.tarafdar2003@gmail.com>
2024-08-10 19:00:46 +0530

    event card update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Slider from "../Components/Slider"; // Adjust the path if necessary
+import Slider from "../Components/Slider";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import Company_card from "../Components/Company_card";
@@ -26,7 +26,6 @@ export default function Home() {
         id="services"
         className="bg-slate-900 w-full h-auto px-20 text-center"
       >
-        {/* event cards */}
         <Event_card />
       </div>
 
@@ -56,38 +55,39 @@ export default function Home() {
         />
 
         {/* FEATURES 2 ------------------------------------------------------------ */}
-        <div id="highlight" className="py-16 px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14">
-            <div className="mt-16 mb-1 grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 lg:gap-10 items-center">
+        <div
+          id="highlight"
+          className=" px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 pb-16"
+        >
+          <div className="mt-16 mb-1 grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 lg:gap-2">
+            {/* Image Slider Column */}
+            <div id="imuu" className="flex justify-center rounded-lg">
+              <Slider />
+            </div>
 
-                  {/* Image Slider Column */}
-                  <div id="imuu" className="flex justify-center rounded-lg">
-                    <Slider />
-                  </div>
+            {/* Text Column */}
+            <div className="flex flex-col text-center md:text-left 2xl:pt-[15%] 2xl:px-14 xl:pt-[12%] lg:pt-[10%]">
+              <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-[45px] xl:text-6xl 2xl:text-7xl font-bold mb-4 font-serif">
+                Discover Our Key Features
+              </h2>
+              <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-[25px] lg:pt-5 md:pt-1">
+                Explore the unique features that set us apart from the
+                competition. Our platform is designed to cater to your every
+                need, ensuring a seamless and successful event experience.
+              </p>
 
-                  {/* Text Column */}
-                  <div className="flex flex-col text-center md:text-left">
-                    <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold mb-4">
-                      Discover Our Key Features
-                    </h2>
-                    <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
-                      Explore the unique features that set us apart from the competition. Our platform is designed to cater to your every need, ensuring a seamless and successful event experience.
-                    </p>
-                  </div>
-
-                  {/* Overlay Image - Not responsive till now */}
-                  <div
-                    style={{
-                      position: "absolute",
-                      top: "357%",
-                      left: "47%",
-                      transform: "translateX(-50%)",
-                      zIndex: 10,
-                    }}
-                  >
-                      <div className="w-[400px] h-12 bg-gradient-to-r from-blue-900 via-slate-900 to-cyan-700 rounded-3xl mt-5 text-white text-2xl flex flex-col items-center">hi shreya</div>
-                      <div className="w-[300px] h-12 bg-gradient-to-r from-blue-900 via-slate-900 to-cyan-700 rounded-3xl mt-5 text-white text-2xl flex flex-col items-center">hi shreya</div>
-                      <div className="w-[200px] h-12 bg-gradient-to-r from-blue-900 via-slate-900 to-cyan-700 rounded-3xl mt-5 text-white text-2xl flex flex-col items-center">hi shreya</div>            
-                  </div> 
+              <div className="md:absolute 2xl:ml-[-17rem] xl:ml-[-10rem] lg:ml-[-10rem] md:ml-[-8rem] 2xl:mt-[25%] xl:mt-[20%] lg:mt-[21%] md:mt-[30%] sm:m-auto">
+                <div className=" 2xl:w-[450px] xl:w-[320px] lg:w-[300px] md:w-[300px] sm:w-[250px] 2xl:h-14 xl:h-11 sm:h-12 bg-gradient-to-r from-slate-800 via-blue-700 to-cyan-700 rounded-3xl mt-5 text-white text-2xl flex justify-center items-center">
+                  10+ Payment Gateway
+                </div>
+                <div className="2xl:w-[350px]  xl:w-[240px] lg:w-[220px] md:w-[220px] sm:w-[100%] 2xl:h-14 xl:h-11 sm:h-12 bg-gradient-to-r from-slate-800 via-blue-700 to-cyan-700 rounded-3xl mt-5 text-white text-2xl flex justify-center items-center">
+                  Reviews Rating
+                </div>
+                <div className="2xl:w-[250px]  xl:w-[140px] lg:w-[140px] md:w-[140px] sm:w-[100%] 2xl:h-14 xl:h-11 sm:h-12 bg-gradient-to-r from-slate-800 via-blue-700 to-cyan-700 rounded-3xl mt-5 text-white text-2xl flex justify-center items-center">
+                  Venue Map
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
@@ -160,6 +160,7 @@ export default function Home() {
 
       {/* Frequently asked question--------------------------------------------------------------- */}
       <FreqtQuestion />
+
       <Footer />
     </>
   );
--- src/components/Event_card.jsx
+++ src/components/Event_card.jsx
@@ -1,86 +1,76 @@
 import React from "react";
+import inPerson_video from "../assets/inPerson_video.mp4";
+import virtual from "../assets/virtual.mp4";
+import hybrid from "../assets/hybrid.mp4";
+
+const Event = ({ event_type, des, video }) => {
+  return (
+    <>
+      <div className="pb-7 bg-gradient-to-r from-indigo-800 via-purple-300 to-blue-600 rounded-xl overflow-hidden">
+        <div className="p-4">
+          <h2 className="text-2xl font-semibold justify-center font-serif">
+            {event_type}
+          </h2>
+          <p className="mt-4 text-white">{des}</p>
+        </div>
+        <div className="pt-2 pb-4">
+          <button className="bg-blue-200 text-black px-4 py-2 rounded-2xl align-middle justify-between">
+            Explore More
+          </button>
+        </div>
+        <div className="h-52 w-full bg-cover bg-center flex justify-center mt-2">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="object-cover overflow-hidden h-52 w-[80%] rounded-lg p-7"
+          >
+            <source src={video} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default function Event_card() {
   return (
     <div className="py-5 flex flex-col items-center justify-center pb-24">
-      <h1 className="text-3xl font-bold mb-6 text-white py-10">
+      <h1 className="text-3xl font-bold mb-6 text-white py-10 pb-12">
         The minimalist way to host all your events successfully!!!
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 sm:gap-y-10 md:gap-x-9 lg:gap-x-9 xl:gap-x-20 2xl:gap-x-28">
-
-      {/* In-person Events--------------------------------------------- */}
-        <div className="bg-gradient-to-r from-indigo-800 via-purple-300 to-blue-600 rounded-xl overflow-hidden bg-gradient-to-br">
-          <div className="p-4">
-            <h2 className="text-xl font-semibold justify-center">
-              In-Person Meeting
-            </h2>
-            <p className="mt-2 text-white">
-              Keep it all together at the venue
-            </p>
-          </div>
-          <div className="p-4">
-            <button className="bg-blue-200 text-black px-4 py-2 rounded-2xl align-middle justify-between">
-              Explore More
-            </button>
-          </div>
-          <div className="h-48 bg-cover bg-center">{/* img */}</div>
-        </div>
+        {/* In-person Events--------------------------------------------- */}
+        <Event
+          event_type="In-Person"
+          des="Keep it all together at the venue"
+          video={inPerson_video}
+        />
 
         {/* Virtual Events--------------------------------------------- */}
-        <div className="bg-gradient-to-r from-indigo-800 via-purple-300 to-blue-600 shadow-md rounded-xl overflow-hidden">
-          <div className="p-4">
-            <h2 className="text-xl font-semibold justify-center">
-              Virtual Meeting
-            </h2>
-            <p className="mt-2 text-white">Go beyond webinars and workshops</p>
-            <div className="p-4">
-              <button className="bg-blue-200 text-black px-4 py-2 rounded-2xl align-middle justify-between">
-                Explore More
-              </button>
-            </div>
-            <div className="h-48 bg-cover bg-center">{/* img */}</div>
-          </div>
-        </div>
+        <Event
+          event_type="Virtual Meeting"
+          des="Go beyond webinars and workshops"
+          video={virtual}
+        />
 
-        {/* Hybrid Event---------------------------------------------------- */}
-        <div className="bg-gradient-to-r from-indigo-800 via-purple-300 to-blue-600 shadow-lg rounded-lg overflow-hidden hidden lg:block">
-          <div className="p-4">
-            <h2 className="text-xl font-semibold justify-center">
-              Hybrid Meeting
-            </h2>
-            <p className="mt-2 text-white">
-              Merge the physical with the virtual
-            </p>
-          </div>
-          <div className="p-4">
-            <button className="bg-blue-200 text-black px-4 py-2 rounded-2xl align-middle justify-between">
-              Explore More
-            </button>
-          </div>
-          <div className="h-48 bg-cover bg-center">
-            {/* img  */}
-          </div>
-        </div>
+        {/* Hybrid Event--------------------------------------------- */}
+        <Event
+          event_type="Hybrid Meeting"
+          des="Merge the physical with the virtual"
+          video={hybrid}
+        />
       </div>
-      
+
+      {/* Onpy For md & sm Screen--------------------------------------- */}
       <div className="md:w-[19rem] h-auto m-autopt-10 block sm:block md:block lg:hidden xl:hidden 2xl:hidden pt-10">
-          <div className="bg-gradient-to-r from-indigo-800 via-purple-300 to-blue-600 shadow-lg rounded-lg overflow-hidden w-full">
-            <div className="p-4">
-              <h2 className="text-xl font-semibold justify-center">
-                Hybrid Meeting
-              </h2>
-              <p className="mt-2 text-white">
-                Merge the physical with the virtual
-              </p>
-            </div>
-            <div className="p-4">
-              <button className="bg-blue-200 text-black px-4 py-2 rounded-2xl align-middle justify-between">
-                Explore More
-              </button>
-            </div>
-            <div className="h-48 bg-cover bg-center">{/* img */}</div>
-          </div>
-        </div>
+        <Event
+          event_type="Hybrid Meeting"
+          des="Merge the physical with the virtual"
+          video={hybrid}
+        />
+      </div>
     </div>
   );
 }
