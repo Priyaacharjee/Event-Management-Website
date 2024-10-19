ab64279e59f51e7fc57107d68e4a9fbc0a509c04
codingqween <jayitabhowmick07@gmail.com>
2024-10-20 01:43:25 +0530

    update

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -23,6 +23,17 @@ export default function Header({ menuItems }) {
 
   const [searchBarClicked, setSearchBarClicked] = useState(false);
   const [isSearchDropdown, setIsSearchDropdown] = useState(false);
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  const handleSignUpClick = () => {
+    navigate("/signup");
+  };
+
+  const handleLogInClick = () => {
+    navigate("/login");
+  };
+
+
 
   const searchClick = () => {
     if (searchBarClicked) {
@@ -36,7 +47,7 @@ export default function Header({ menuItems }) {
     }
     setDropDownOpen(false);
     setHamburgerMenuClicked(false);
-  }
+  };
 
   const hambergerClick = () => {
     if (hamburgerMenuClicked) {
@@ -122,88 +133,6 @@ export default function Header({ menuItems }) {
         {/* ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------  NAVBAR */}
         <nav
           id="header"
-<<<<<<< HEAD
-          className="h-12 flex items-center px-4 justify-center w-full text-[16px] bg-black text-white"
-        >
-          {/* Logo */}
-          <div className="mt-[1rem] w-[50%] 2xl:w-[20%] xl:w-[20%] lg:w-[20%] md:w-[20%] sm:w-[15%] lg:pl-15 xl:pl-20">
-            <img className="w-[70%] h-[30%] rounded-2xl" src="logo.jpeg" />
-          </div>
-
-          {/* Navbar Menu */}
-          <div className="items-center md:w-3/5 hidden sm:block md:block lg:block xl:block 2xl:block sm:text-sm md:text-sm lg:text-lg sm:w-[45%] bg-slate-500">
-            <ul className="text-center grid grid-cols-4 gap-1">
-              <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline">
-                Services
-              </li>
-              <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline">
-                About
-              </li>
-              <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline">
-                Contact
-              </li>
-              <li className="hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline">
-                Gallery
-              </li>
-            </ul>
-          </div>
-
-          {/* User logo */}
-          <div className="w-2/5 2xl:w-1/5 xl:w-1/5 lg:w-1/5 md:w-1/5 sm:w-[35%] flex justify-end py-1 items-center 2xl:pl-[6%] xl:pl-[6%] lg:pl-[5%] md:pl-[5%] sm:pl-[5%] pl-[5%]  sm:ml-12 bg-blue-600">
-            {/* Search Button */}
-            <div className="2xl:w-[100%] xl:w-[35%] lg:w-[40%] md:w-[100%] sm:w-[100%] md:mr-[-5%] sm:ml-[2%] sm:pl-[3%] pl-[5%] flex justify-center pr-1 lg:ml-[-78px] bg-red-500">
-              <img
-                src="https://img.icons8.com/?size=100&id=KPmthqkeTgDN&format=png&color=000000"
-                alt=""
-                title="Search-bar"
-                className="rounded-xl h-6 w-6 md:h-8 md:w-8 lg:h-8 lg:w-8 xl:h-8 xl:w-8 2xl:h-8 2xl:w-8 sm:h-7 sm:w-8 hover:cursor-pointer md:ml-1 sm:ml-5"
-                onClick={searchClick}
-              />
-            </div>
-            {/* <div className="w-auto pr-5">
-              {/* <span className="text-white font-bold hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline">
-              </span> 
-            </div> */}
-
-            {/* User Image */}
-            <div className="2xl:w-[100%] xl:w-[35%] lg:w-[40%] md:w-[100%] flex justify-center pr-1 mr-1 ml-6 md:ml-[24%]">
-              <img
-                src="https://cdn-icons-png.freepik.com/512/219/219986.png"
-                alt=""
-                title="User Image"
-                className="rounded-xl h-6 w-6 md:h-8 md:w-8 lg:h-8 lg:w-8 xl:h-8 xl:w-8 2xl:h-8 2xl:w-8 sm:h-6 sm:w-6 hover:cursor-pointer"
-              />
-            </div>
-            <div className="w-auto pr-2">
-              <span className="text-white font-bold hover:cursor-pointer hover:text-blue-100 hover:font-bold hover:underline">
-                User
-              </span>
-            </div>
-            <div className="sm:w-[40%] w-[30%]">
-              {dropDownOpen ? (
-                <FontAwesomeIcon
-                  icon={faCaretUp}
-                  className="hover:cursor-pointer hover:size-4"
-                  style={{ color: "#ffffff" }}
-                  onClick={dropDown}
-                />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faCaretDown}
-                  style={{ color: "#ffffff" }}
-                  className="hover:cursor-pointer hover:size-4"
-                  onClick={dropDown}
-                />
-              )}
-            </div>
-
-            <div className="mt-6 ml-6">
-              <ThemeButton />
-            </div>
-
-            {/* Hamburger Menu */}
-            {hamburgerMenuClicked ? (
-=======
           className="h-16 flex items-center px-4 justify-between w-full text-[16px] bg-black text-white fixed top-0 z-50"
         >
           {/* Logo */}
@@ -232,7 +161,6 @@ export default function Header({ menuItems }) {
           <div className="w-[50%] sm:w-[35%] md:w-[35%] lg:w-2/5 xl:w-[25%] 2xl:w-[20%] flex justify-end items-center space-x-4">
             {/* Search Button */}
             <div className="hidden lg:flex xl:w-[35%] lg:w-[20%] md:w-full justify-center">
->>>>>>> 0b6e19035f4ae1f974d8456fbfb071db52e135b0
               <FontAwesomeIcon
                 icon={faMagnifyingGlass}
                 style={{ color: "#ffffff" }}
@@ -295,29 +223,6 @@ export default function Header({ menuItems }) {
           </div>
         </nav>
 
-<<<<<<< HEAD
-        {/* Search Bar Dropdown */}
-        {(searchBarClicked || isSearchDropdown) && (
-          <div
-            className={`absolute top-14 right-20 flex-col flex text-white w-40 items-center mr-[5%] sm:mr-[5%] md:mr-[5%] lg:mr-[5%] bg-slate-300 bg-opacity-[0.3] rounded-lg ${isSearchDropdown ? "animate-slideUp" : "animate-slideBelow"
-              }`}
-          >
-            <input
-              className="bg-zinc-200 text-zinc-600 font-mono ring-1 ring-zinc-400 focus:ring-2 focus:ring-blue-800 outline-none duration-300 placeholder:text-zinc-600 placeholder:opacity-50 rounded-full px-4 py-2 shadow-md focus:shadow-lg focus:shadow-blue-800 dark:shadow-md dark:shadow-purple-500"
-              autoComplete="off"
-              placeholder="Search here..."
-              name="text"
-              type="text"
-            />
-          </div>
-        )}
-
-        {/* User Dropdown */}
-        {(dropDownOpen || isClosingDropdown) && (
-          <div
-            className={`absolute top-14 left-[86%] flex-col flex text-white w-40 items-center h-[5.2rem] mr-[5%] sm:mr-[5%] md:mr-[3%] lg:mr-[5%] bg-slate-300 bg-opacity-[0.3] rounded-lg ${isClosingDropdown ? "animate-slideUp" : "animate-slideBelow"
-              }`}
-=======
         {/* Hamburger Menu */}
         {(hamburgerMenuClicked || isClosing) && (
           <div
@@ -347,7 +252,6 @@ export default function Header({ menuItems }) {
             className={`border-4 absolute top-14 left-[49%] 2xl:left-[87%] xl:left-[83%] lg:left-[75%] md:left-[71%] sm:left-[66%] flex-col flex text-white w-40 items-center h-[5.2rem] mr-[5%] sm:mr-[5%] md:mr-[3%] lg:mr-[5%] bg-slate-300 bg-opacity-[0.3] rounded-lg ${
               isClosingDropdown ? "animate-slideUp" : "animate-slideBelow"
             }`}
->>>>>>> 0b6e19035f4ae1f974d8456fbfb071db52e135b0
           >
             <div className="w-full text-center pt-2 pb-2 hover:cursor-pointer hover:text-blue-300 hover:underline hover:font-bold">
               My Account
@@ -358,16 +262,6 @@ export default function Header({ menuItems }) {
           </div>
         )}
 
-<<<<<<< HEAD
-        {/* Hamburger Menu Dropdown */}
-        {(hamburgerMenuClicked || isClosing) && (
-          <div
-            className={`flex-col flex justify-end mr-5 text-white w-40 items-center h-[12.2rem] ${isClosing ? "animate-slideOut" : "animate-slideIn"
-              } absolute top-14 right-5 bg-slate-300 bg-opacity-[0.3] rounded-lg`}
-          >
-            <div className="w-full text-center pt-2 pb-2 border-b-[0.5px] border-white hover:cursor-pointer hover:text-blue-300 hover:underline hover:font-bold">
-              Home
-=======
  
       </div>
 
@@ -421,7 +315,6 @@ export default function Header({ menuItems }) {
               >
                 Create Events
               </button>
->>>>>>> 0b6e19035f4ae1f974d8456fbfb071db52e135b0
             </div>
             <div className="flex items-center justify-center w-[12rem] lg:w-[12rem] h-[3rem]" style={{ color: "#ffffff" }}>
               <button
