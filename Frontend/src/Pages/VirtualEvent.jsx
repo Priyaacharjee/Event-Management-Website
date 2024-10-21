658d5dabede02d988015980df958b663b1da66ba
codingqween <jayitabhowmick07@gmail.com>
2024-10-21 21:11:46 +0530

    update

--- Frontend/src/Pages/VirtualEvent.jsx
+++ Frontend/src/Pages/VirtualEvent.jsx
@@ -79,7 +79,7 @@ function VirtualEvent() {
       {/* Upcoming Events Section */}
       <section className="text-center mb-8">
         <h2 className="text-3xl font-serif font-semibold">
-          Our upcoming events
+          Our upcoming events!!
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 mt-8">
           {Array.isArray(virtualEvents) &&
