6ebee6aa14e2d2d26c3710ffdff313ae79aadf7b
codingqween <jayitabhowmick07@gmail.com>
2024-12-12 03:41:29 +0530

    UPDATE

--- Frontend/src/Pages/Venue_Details.jsx
+++ Frontend/src/Pages/Venue_Details.jsx
@@ -25,44 +25,14 @@ function Venue_Details() {
          <div className="App">
         {/* Header Section */}
         <Navbar menuItems={headerMenuItems} />
-          <div
-            style={{
-              backgroundImage:
-                "url(https://media.istockphoto.com/id/910193030/photo/modern-minimalist-villa.jpg?s=612x612&w=0&k=20&c=xDSV332OhNpQaBGRTSExfjSgJJ5Lmdgh9p479mQ4fkw=)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              height: "40vh",
-              width: "100%",
-            }}
-          > 
+        
           <div
             className="text-7xl text-orange-950 font-bold font-serif text-center "
           >
             Venue Details!!
           </div>         
             </div>
-    </div>
-      <div className="p-6 bg-gray-100 min-h-screen">
-        <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-lg">
-          <h1 className="text-2xl font-bold mb-4">{venue.name}</h1>
-          <p className="text-gray-700">City: {venue.city}</p>
-          <p className="text-gray-700">Max Headcount: {venue.maxHead}</p>
-          <p className="text-gray-700">Email: {venue.email}</p>
-          <p className="text-gray-700">Contact: {venue.contact}</p>
-          <p className="text-gray-700 mt-4">Description: {venue.description}</p>
-          <div className="mt-6 grid grid-cols-2 gap-4">
-            {venue.gallery.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt={`Gallery image ${index + 1}`}
-                className="rounded-lg shadow-md"
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-
+      
 <div className="m-0 p-0" id="contact">
 <Footer menuItems1={footerMenuItems} />
 </div>
