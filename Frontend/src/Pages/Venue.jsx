9eede0db0d7338a71875b21bb56a5f2e87f5b568
Keya Tarafdar <keya.tarafdar2003@gmail.com>
2024-12-12 20:59:08 +0530

    update

--- Frontend/src/Pages/Venue.jsx
+++ Frontend/src/Pages/Venue.jsx
@@ -12,7 +12,7 @@ const footerMenuItems = [
 ];
 function Venue() {
   const navigate = useNavigate();
-  
+
   const headerMenuItems = [
     { label: "Home", to: "/" },
     { label: "About", to: "/" },
@@ -25,34 +25,29 @@ function Venue() {
       <div className="App">
         {/* Header Section */}
         <Navbar menuItems={headerMenuItems} />
-          <div
-            style={{
-              backgroundImage:
-                "url(https://media.istockphoto.com/id/910193030/photo/modern-minimalist-villa.jpg?s=612x612&w=0&k=20&c=xDSV332OhNpQaBGRTSExfjSgJJ5Lmdgh9p479mQ4fkw=)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              height: "40vh",
-              width: "100%",
-            }}
-          > 
-          <div
-            className="text-7xl text-orange-950 font-bold font-serif text-center "
-          >
+        <div
+          style={{
+            backgroundImage:
+              "url(https://media.istockphoto.com/id/910193030/photo/modern-minimalist-villa.jpg?s=612x612&w=0&k=20&c=xDSV332OhNpQaBGRTSExfjSgJJ5Lmdgh9p479mQ4fkw=)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "40vh",
+            width: "100%",
+          }}
+        >
+          <div className="text-7xl text-orange-950 font-bold font-serif flex items-center justify-center h-[40vh]">
             Venue Details!!
-          </div>         
-            </div>
-    </div>
+          </div>
+        </div>
+      </div>
 
-   {/*EVENT TYPES --------------------------------------------------------- */}
-   <div
-        className="h-auto px-8 rounded-[2rem] text-center mt-8 lg:mt-0 ml-12 mr-12"
-      >
+      <div className="h-auto px-8 rounded-[2rem] text-center mt-8 lg:mt-0 ml-12 mr-12">
         <Venue_card />
       </div>
-<div className="m-0 p-0" id="contact">
-<Footer menuItems1={footerMenuItems} />
-</div>
-</>
+      <div className="m-0 p-0" id="contact">
+        <Footer menuItems1={footerMenuItems} />
+      </div>
+    </>
   );
 }
 export default Venue;
